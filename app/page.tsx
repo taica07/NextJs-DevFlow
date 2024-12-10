410dbe224501de6b2118849acefe3fcceea6fc88
taica84 <taica_rules@yahoo.com>
2024-12-11 01:13:28 +0200

    Tailwind CSS setup & theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-
-
 const Page = () => {
-    return (
-        <div>
-            <h1 className='text-3xl text-orange-700 '>Welcome to the world of Next js</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-primary-500 text-3xl ">
+        Welcome to the world of Next js
+      </h1>
+      <p>Lets get started</p> <span>how should i proceed</span>
+    </div>
+  );
 };
 
 export default Page;
