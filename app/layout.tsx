44f06e7ae8c35dc6a81737bae578aa6d248eb65e
taica84 <taica_rules@yahoo.com>
2024-12-12 01:01:01 +0200

    implement Github Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,6 @@ import React from "react";
 
 import "./globals.css";
 import ThemeProvider from "@/context/Theme";
-import Navbar from "@/components/navigation/navbar/Navbar";
 
 const inter = localFont({
   src: "./fonts/InterVF.ttf",
@@ -43,7 +42,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
